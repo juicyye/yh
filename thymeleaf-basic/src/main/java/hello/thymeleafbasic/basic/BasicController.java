0d054742c6584bb300412fa42e8b22e044b898f7
param <spsummerdoc@gmail.com>
2024-03-22 00:05:55 +0900

    thymeleaf_end

---
+++ thymeleaf-basic/src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -0,0 +1,85 @@
+package hello.thymeleafbasic.basic;
+
+import lombok.Data;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+
+    @GetMapping("/text-basic")
+    public String textBasic(Model model) {
+        model.addAttribute("data", "hello spring!!");
+        return "basic/text-basic";
+    }
+
+    @GetMapping("/variable.html")
+    public String variable(Model model){
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA",userA);
+        map.put("userB", userB);
+
+        model.addAttribute("list", list);
+        model.addAttribute("map", map);
+        return "basic/variable.html";
+
+    }
+
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
+    }
+
+    @GetMapping("/attribute")
+    public String attribute() {
+        return "basic/attribute";
+    }
+    @GetMapping("/fragment")
+    public String template() {
+        return "template/fragment/fragmentMain";
+    }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend(){
+        return "layout/layoutExtend/layoutExtendMain";
+    }
+
+    @Data
+    static class User {
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
+}
